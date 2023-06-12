064bca196be518a1f3fb486f8515d0e7f53f327f
OlenkaSenyk <olena.senyk16@gmail.com>
2023-06-12 15:51:23 +0300

    base-commit

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
-require('dotenv').config();
+const { Telegraf, Markup } = require("telegraf");
+const { message } = require("telegraf/filters");
+require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.start(async (ctx) => {
+  try {
+    await ctx.reply(
+      "Welcome",
+      Markup.inlineKeyboard([
+        [Markup.button.callback("Є улюбленець", "petOwnerBtn")],
+        [Markup.button.callback("Нема улюбленця", "guestBtn")],
+        [Markup.button.callback("Донати", "donatesBtn")],
+      ])
+    );
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+bot.action("petOwnerBtn", async (ctx) => {
+  try {
+    await ctx.answerCbQuery();
+    await ctx.reply(
+      "Що тебе цікавить?",
+      Markup.inlineKeyboard([
+        [Markup.button.callback("...", "Btn1")],
+        [Markup.button.callback("...", "Btn2")],
+        [Markup.button.callback("..", "Btn3")],
+      ])
+    );
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
