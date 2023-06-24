c34d17e977f49f86d98995b1db2c81ee7ac6ac71
Anastasiia Kushnirova <90209373+Anastasiia20122002@users.noreply.github.com>
2023-06-24 18:00:05 +0300

    bot-command.js commit

---
+++ src/bot-command.js
@@ -0,0 +1,45 @@
+import { Markup } from "telegraf";
+import { emojis_obj } from "./emojis/constants-emojis.js";
+async function botCommand(ctx, text, inline_keyboard, start) {
+    if(global.start){
+      await ctx.replyWithPhoto({
+        source: "poster.jpg",
+      });
+  
+      global.start_msg = await ctx.replyWithHTML(
+        'Привіт, любий друже '+emojis_obj.smile+'\nТебе вітає телеграм-бот притулку "Сіріус" '+emojis_obj.dog+" "+emojis_obj.cat,
+        {
+          disable_web_page_preview: true,
+        }
+      );
+  
+      global.main_msg = await ctx.replyWithHTML(
+      text,
+      Markup.inlineKeyboard(inline_keyboard))
+      global.start=false;
+    }
+    else{
+      if (global.start_msg_cnt) {
+        await ctx.telegram.deleteMessage(ctx.chat.id, global.start_msg.message_id);
+        global.start_msg_cnt = false;
+      }
+      global.main_msg = await ctx.telegram.editMessageText(
+        ctx.chat.id,
+        global.main_msg.message_id,
+        null,
+        text,
+        null,
+        null
+      );
+    
+      await ctx.telegram.editMessageReplyMarkup(
+        ctx.chat.id,
+        global.main_msg.message_id,
+        null,
+        {
+          inline_keyboard,
+        }
+      );
+      }
+  }
+  export {botCommand}
